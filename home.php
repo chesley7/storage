5c52ab1e05eaaac47b831d603047652e370a2ec4
chesley7 <chesley7@gmail.com>
2014-06-20 15:18:11 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,17 @@
 <!DOCTYPE HTML>
+
+<!--                                                                                    
+FILE NAME: home.php                                                                     
+WRITTEN BY: Chesley                                                                     
+WHEN: June 20 2014                                                                      
+PURPOSE: homepage for storage                                                           
+-->
+
 <html>
 <head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Storage Home</title>
+  <link type="text/css" href="style.css" rel="stylesheet"/>
 </head>
 <body>
 hi.
@@ -8,7 +19,6 @@ hi.
 </html>
 
 <?php
-$dbh = db_connect($MYSQL5_316376_smartcube); //connect to the long-named database
+//$dbh = db_connect($MYSQL5_316376_smartcube); //connect to the long-named database
 
 ?>
-
